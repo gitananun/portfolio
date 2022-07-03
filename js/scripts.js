365b9653c12afb753a5833fcb0000e4023b0f9fa
gitananun <tigran.muradyan140201@gmail.com>
2022-07-03 21:22:12 +0400

    Improvements

--- js/scripts.js
+++ js/scripts.js
@@ -77,3 +77,14 @@
 
   $('.experience-in-years').html(new Date().getFullYear() - 2017);
 })(jQuery);
+
+document.addEventListener('mousemove', parallax);
+function parallax(event) {
+  this.querySelectorAll('.mouse').forEach((shift) => {
+    const position = shift.getAttribute('value');
+    const x = (window.innerWidth - event.pageX * position) / 90;
+    const y = (window.innerHeight - event.pageY * position) / 90;
+
+    shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+}
